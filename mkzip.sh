39693710a2ee8dc72505a85cf5ccf60ca35e0074
Kaedehito <pik6cs@gmail.com>
2025-01-30 15:54:53 +0900

    add mkzip.sh

---
+++ mkzip.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+version="1"
+output_dir="out"
+mkdir -p "$output_dir"  # out ディレクトリを作成（存在してもOK）
+
+current_time=$(date "+%Y-%m-%d-%H:%M:%S")
+
+zip_name="$output_dir/main.zip"
+
+# 現在のディレクトリ内のディレクトリを取得（outを除外）
+dirs=()
+for dir in */; do
+    if [ -d "$dir" ] && [ "$dir" != "$output_dir/" ]; then
+        dirs+=("$dir")  # 配列に追加
+    fi
+done
+
+# ディレクトリが存在する場合のみ圧縮
+if [ ${#dirs[@]} -gt 0 ]; then
+    echo "Compressing directories into $zip_name..."
+    zip -r "$zip_name" "${dirs[@]}"
+    echo "Created: $zip_name"
+    cat <<EOF > "$output_dir/info.json"
+{
+	"version": $version,
+	"date": $current_time,
+	"file": $zip_name,
+}
+EOF
+
+else
+    echo "No directories found to compress."
+fi
+
