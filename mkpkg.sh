2f6b55f23f4f1b7f757cae063e0e7ae9b5cfc409
Kaedehito <pik6cs@gmail.com>
2025-01-30 15:38:10 +0900

    add hello_lade package and add mkpkg.sh

---
+++ mkpkg.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+echo -n "package name? "
+read pkgname
+echo -n "version? "
+read version
+echo -n "description? "
+read desc
+echo -n "license? "
+read license
+echo -n "authors? "
+read authors
+echo -n "dependencies? (comma-separated) "
+read depends
+echo -n "repository? "
+read repo
+echo -n "download? (y/n) "
+read download
+
+mkdir -p "$pkgname"
+
+cat <<EOF > "$pkgname/info.toml"
+name = "$pkgname"
+version = "$version"
+description = "$desc"
+license = "$license"
+authors = ["$authors"]
+dependencies = [$(echo "$depends" | sed 's/, */", "/g' | sed 's/^/"/' | sed 's/$/"/')]
+repository = "$repo"
+EOF
+
+if [ "$download" = "y" ]; then
+    echo "download = true" >> "$pkgname/info.toml"
+fi
+
