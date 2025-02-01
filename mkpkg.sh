b5d85f6e83dbe7a9fe7ffd61f8dce7b07570d4d6
Kaedehito <pik6cs@gmail.com>
2025-02-01 14:09:28 +0900

    commit

--- mkpkg.sh
+++ mkpkg.sh
@@ -2,7 +2,9 @@
 
 echo -n "package name? "
 read pkgname
-echo -n "version? (comma-separated) "
+echo -n "older_version? (y/n) "
+read older_y_n
+echo -n "version? "
 read version
 echo -n "description? "
 read desc
@@ -21,7 +23,8 @@ mkdir -p "$pkgname"
 
 cat <<EOF > "$pkgname/info.toml"
 name = "$pkgname"
-version = [$(echo "$version" | sed 's/, */", "/g' | sed 's/^/"/' | sed 's/$/"/')]
+version = "$version"
+older_version = [$(echo "$older_version" | sed 's/, */", "/g' | sed 's/^/"/' | sed 's/$/"/')]
 description = "$desc"
 license = "$license"
 authors = [$(echo "$authors" | sed 's/, */", "/g' | sed 's/^/"/' | sed 's/$/"/')]
@@ -33,5 +36,11 @@ if [ "$download" = "y" ]; then
     echo -n "url? "
     read url
     echo "download = \"$url\"" >> "$pkgname/info.toml"
+fi
+
+if [ "$older_y_n" = "y" ]; then
+    echo -n "older_version? (comma-separated) "
+    read older
+    echo "dependencies = [$(echo "$older" | sed 's/, */", "/g' | sed 's/^/"/' | sed 's/$/"/')]" >> "$pkgname/info.toml"
 fi
 
